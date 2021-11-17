d1e2ef5c7a57e6186c9e1760055e4badf9ca808f
Ricardo Silva <ricardo.silva@3decide.com>
2021-11-17 17:20:07 +0000

    switches BrowserRouter for HashRouter to run on github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Loading from './components/Loading';
 
